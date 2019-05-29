f82737aafe30eac8d180bfc67e39e1c0b77b0ec8
ellenchuaye <chuayunen@gmail.com>
2019-05-29 14:09:15 -0700

    Update app.vue

--- src/components/app.vue
+++ src/components/app.vue
@@ -14,16 +14,6 @@
   </f7-panel>
 
 
-  <!-- Right panel with reveal effect-->
-  <f7-panel right reveal theme-dark>
-    <f7-view>
-      <f7-page>
-        <f7-navbar title="Right Panel"></f7-navbar>
-        <f7-block>Right panel content goes here</f7-block>
-      </f7-page>
-    </f7-view>
-  </f7-panel>
-
 
   <!-- Your main view, should have "view-main" class -->
   <f7-view main class="safe-areas" url="/"></f7-view>
@@ -45,35 +35,7 @@
     </f7-view>
   </f7-popup>
 
-  <f7-login-screen id="my-login-screen">
-    <f7-view>
-      <f7-page login-screen>
-        <f7-login-screen-title>Login</f7-login-screen-title>
-        <f7-list form>
-          <f7-list-input
-            type="text"
-            name="username"
-            placeholder="Your username"
-            :value="username"
-            @input="username = $event.target.value"
-          ></f7-list-input>
-          <f7-list-input
-            type="password"
-            name="password"
-            placeholder="Your password"
-            :value="password"
-            @input="password = $event.target.value"
-          ></f7-list-input>
-        </f7-list>
-        <f7-list>
-          <f7-list-button title="Sign In" login-screen-close @click="alertLoginData"></f7-list-button>
-          <f7-block-footer>
-            Some text about login information.<br>Click "Sign In" to close Login Screen
-          </f7-block-footer>
-        </f7-list>
-      </f7-page>
-    </f7-view>
-  </f7-login-screen>
+
 </f7-app>
 </template>
 <script>
@@ -87,7 +49,7 @@
         f7params: {
           id: 'io.framework7.myapp', // App bundle ID
           name: 'Oakland First Friday', // App name
-          theme: 'auto', // Automatic theme detection
+          theme: 'aurora', // Automatic theme detection
           // App root data
           data: function () {
             return {
